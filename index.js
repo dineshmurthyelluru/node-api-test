31b44b51913ceef0b5cff688c59147a827bd3988
dineshmurthyelluru <108884363+dineshmurthyelluru@users.noreply.github.com>
2022-07-07 20:52:06 +0530

    test

---
+++ index.js
@@ -0,0 +1,13 @@
+const express =require('express');
+const port = process.env.PORT || 1420
+
+
+const app=express()
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Welcome</h1>')
+})
+
+app.listen(port,()=>{
+    console.log('**** Application is running',port);
+})
